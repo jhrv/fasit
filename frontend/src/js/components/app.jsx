c9f67347c5a6847797c0e5e39dc617d5bf03fed0
Johnny Horvi <johnny.horvi@gmail.com>
2015-10-13 12:45:57 +0200

    initial commit

---
+++ frontend/src/js/components/app.jsx
@@ -0,0 +1,26 @@
+var React  = require('react');
+var ReactDOM  = require('react-dom');
+
+var Skya = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <nav className="navbar navbar-fixed-top sera-header">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <a className="navbar-brand" href="#">
+                                <span className="fa-stack fa-lg">
+                                    <i className="fa fa-square fa-stack-2x logo"></i>
+                                    <strong className="fa-stack-1x fa-stack-text fa-comment-text ">S</strong>
+                                </span>
+                                &nbsp;sera</a>
+                        </div>
+                    </div>
+                </nav>
+                Her kommer innhold giitt
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(<Skya />, document.getElementById("content"));
---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+var source = require('vinyl-source-stream')
+var buffer = require('vinyl-buffer')
+var gulp = require('gulp')
+var gutil = require('gulp-util')
+var browserify = require('browserify')
+var reactify = require('reactify')
+var size = require('gulp-size')
+var watchify = require('watchify')
+var notify = require("gulp-notify")
+var gulpif = require('gulp-if')
+var uglify = require('gulp-uglify')
+var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
+
+var env = process.env.NODE_ENV || 'development'
+
+var paths = {
+    entryPoint: './frontend/src/js/components/app.jsx',
+    js: ['./frontend/src/js/**/*.jsx', './app.jsx'],
+    jsLibs: './frontend/src/lib/**/*.js',
+    css: './frontend/src/css/**/*.css',
+    fonts: ['./frontend/src/fonts/**/*', './node_modules/font-awesome/fonts/**/*'],
+    extCss: './frontend/src/ext/css/**/*.css',
+    buildDir: './frontend/build',
+    jsBuild: './frontend/build/js',
+    cssBuild: './frontend/build/css',
+    fontsBuild: './frontend/build/fonts',
+    distDir: './dist',
+    indexHtml: './frontend/src/index.html',
+    favicon: './frontend/src/favicon.png'
+}
+
+var buildScript = function (file, watch) {
+    var props = watchify.args
+    props.entries = [file]
+    props.debug = true
+    
+    var bundler = watch ? watchify(browserify(props)) : browserify(props)
+
+    bundler.transform(reactify)
+
+    var rebundle = function () {
+        var errorHandler = notify.onError({
+            title: "Compile Error",
+            message: "<%= error.message %>"
+        })
+        var stream = bundler.bundle()
+        return stream.on('error', errorHandler)
+           .pipe(source('app.js'))
+           .pipe(buffer())
+           .pipe(gulpif(env === 'production', uglify()))
+           .pipe(size())
+           .pipe(gulp.dest(paths.buildDir + '/'))
+    }
+
+    bundler.on('update', function() {
+        gutil.log('rebundling...')
+        rebundle()
+    })
+
+    return rebundle()
+}
+
+gulp.task('bundle-css', function () {
+    return gulp.src(['./node_modules/font-awesome/css/font-awesome.css', paths.extCss, paths.css])
+        .pipe(concat('bundle.css'))
+        .pipe(minifyCSS())
+        .pipe(size())
+        .pipe(gulp.dest(paths.cssBuild));
+});
+
+gulp.task('build', function() {
+    return buildScript(paths.entryPoint, false)
+})
+
+gulp.task('default', ['build'], function() {
+    return buildScript(paths.entryPoint, true)
+})
