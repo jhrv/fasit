cb3e57b22d1eaa42407b90c78d13d6453d66f3de
Johnny Horvi <johnny.horvi@gmail.com>
2015-10-14 14:51:20 +0200

    rammeverk oppe, endret navn til fasit

--- frontend/src/js/components/app.jsx
+++ frontend/src/js/components/app.jsx
@@ -1,7 +1,11 @@
-var React  = require('react');
-var ReactDOM  = require('react-dom');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router')
+var Router = ReactRouter.Router
+var Route = ReactRouter.Route
+var Link = ReactRouter.Link
 
-var Skya = React.createClass({
+var App = React.createClass({
     render: function () {
         return (
             <div>
@@ -11,16 +15,57 @@ var Skya = React.createClass({
                             <a className="navbar-brand" href="#">
                                 <span className="fa-stack fa-lg">
                                     <i className="fa fa-square fa-stack-2x logo"></i>
-                                    <strong className="fa-stack-1x fa-stack-text fa-comment-text ">S</strong>
+                                    <strong className="fa-stack-1x fa-stack-text fa-comment-text ">F</strong>
                                 </span>
-                                &nbsp;skya</a>
+                                &nbsp;<strong>fasit</strong>
+                            </a>
+                        </div>
+                        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul className="nav navbar-nav">
+                                <li><Link to="/">applications</Link></li>
+                                <li><Link to="/">environments</Link></li>
+                                <li><Link to="/">resources</Link></li>
+                                <li><Link to="/nodes">nodes</Link></li>
+                            </ul>
                         </div>
                     </div>
                 </nav>
-                Her kommer innhold
+
+                <div className="container">
+                    {this.props.children}
+                </div>
             </div>
         )
     }
 });
 
-ReactDOM.render(<Skya />, document.getElementById("content"));
+var Nodes = React.createClass({
+
+    fetchNodes: function(){
+        return [{"hostname": "node1.domain.com"},
+                {"hostname": "node2.domain.com"},
+                {"hostname": "node3.domain.com"}]
+    },
+    
+    render: function(){
+        return (
+                <ul>
+                    {this.fetchNodes().map(function(node){return <li key={node.hostname}><Link to={"/nodes/" + node.hostname}>{node.hostname}</Link></li>})}
+                </ul>
+    )}
+});
+
+var Node = React.createClass({
+    render: function(){
+        return (
+                <h1>{this.props.params.node}</h1>
+        )}
+})
+
+ReactDOM.render((
+    <Router>
+        <Route path="/" component={App}>
+            <Route path="/nodes" component={Nodes} />
+            <Route path="/nodes/:node" component={Node} />
+        </Route>
+    </Router>), document.getElementById("content"));
